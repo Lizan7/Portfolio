b4d6a4dcc587130ce4f8a61790e7b84097f50528
Lizan7 <lizanstha777@gmail.com>
2024-08-23 23:06:44 +0545

    Added skill section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      
+      You can reach me through my contact details:
     </div>
   )
 }
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Skills = () => {
     return (
         <div>
-
+            All my skills are mentioned below:
+            <p>React</p>
+            <p>Node</p>
+            <p>Java</p>
+            <p>Python</p>
+            <p>React</p>
         </div>
     )
 }
