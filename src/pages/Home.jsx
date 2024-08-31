8f65e9b146170131d1f494ed7ee169315fefa33b
Lizan7 <lizanstha777@gmail.com>
2024-08-31 23:23:52 +0545

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,8 @@ const Home = () => {
                 {/* left side div */}
                 <div>
                     <h1 className='text-black'>Lizan Shrestha</h1>
+                    <p>Dream Big so that you can achieve it.</p>
+                    <button className='px-4 py-2 bg-blue-600'>Download CV</button>
                 </div>
                 {/* Right side div */}
                 <div>
